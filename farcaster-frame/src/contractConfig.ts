b3b5759d5936a590f99fc75f9470b863f97f298e
realyyt <emmyisaac60@gmail.com>
2024-09-04 23:18:50 +0530

    feat: abi inclusion

--- farcaster-frame/src/contractConfig.ts
+++ farcaster-frame/src/contractConfig.ts
@@ -1,4 +1,42 @@
 export const contractAddress = "YOUR_CONTRACT_ADDRESS_HERE";
 export const contractABI = [
-  // Your contract ABI here
+  {
+    "inputs": [],
+    "name": "mint",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
 ];
