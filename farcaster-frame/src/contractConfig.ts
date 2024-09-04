1a4b4970320139ccb88401847fc9192dff14b3e2
realyyt <emmyisaac60@gmail.com>
2024-09-04 23:18:55 +0530

    feat: abi inclusion

--- farcaster-frame/src/contractConfig.ts
+++ farcaster-frame/src/contractConfig.ts
@@ -1,42 +1,46 @@
 export const contractAddress = "YOUR_CONTRACT_ADDRESS_HERE";
 export const contractABI = [
   {
-    "inputs": [],
-    "name": "mint",
+    "inputs": [
+      {"internalType": "bytes32", "name": "_commitment", "type": "bytes32"},
+      {"internalType": "uint256", "name": "_amount", "type": "uint256"},
+      {"internalType": "uint256", "name": "_delay", "type": "uint256"}
+    ],
+    "name": "scheduleTransfer",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
   },
   {
     "inputs": [
-      {
-        "internalType": "address",
-        "name": "owner",
-        "type": "address"
-      }
+      {"internalType": "uint[2]", "name": "a", "type": "uint[2]"},
+      {"internalType": "uint[2][2]", "name": "b", "type": "uint[2][2]"},
+      {"internalType": "uint[2]", "name": "c", "type": "uint[2]"},
+      {"internalType": "uint[2]", "name": "input", "type": "uint[2]"},
+      {"internalType": "bytes32", "name": "_commitment", "type": "bytes32"}
     ],
-    "name": "balanceOf",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
     "type": "function"
   },
   {
-    "inputs": [],
-    "name": "totalSupply",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
+    "inputs": [
+      {"internalType": "address", "name": "_creator", "type": "address"},
+      {"internalType": "uint256", "name": "_amount", "type": "uint256"},
+      {"internalType": "string", "name": "_encryptedQuestion", "type": "string"},
+      {"internalType": "bytes32", "name": "_commitment", "type": "bytes32"}
     ],
-    "stateMutability": "view",
+    "name": "askQuestion",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{"internalType": "bytes32", "name": "_questionId", "type": "bytes32"}],
+    "name": "answerQuestion",
+    "outputs": [],
+    "stateMutability": "nonpayable",
     "type": "function"
   }
 ];
